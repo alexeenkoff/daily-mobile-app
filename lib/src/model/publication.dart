7e9b0244fc02692283b6200b5958cc5a7df1e17c
alexeenkoff <alexeenkoff@gmail.com>
2020-02-29 13:28:26 +0300

    create project

---
+++ lib/src/model/publication.dart
@@ -0,0 +1,9 @@
+class Publication {
+  final String id;
+  final String name;
+  final String image;
+  final bool enabled;
+  final String twitter;
+
+  Publication(this.id, this.name, this.image, this.enabled, this.twitter);
+}
