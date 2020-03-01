ed514b8b88716b18b046348f2d3141cfced2a547
alexeenkoff <alexeenkoff@gmail.com>
2020-03-01 23:33:56 +0300

    Start work on theme

---
+++ lib/app_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+ThemeData appTheme() => ThemeData(
+    brightness: Brightness.dark,
+    accentColor: Colors.blueGrey,
+    primaryColor: Color.fromARGB(255, 48, 50, 55),
+    backgroundColor: Color.fromARGB(255, 28, 30, 33),
+    );
