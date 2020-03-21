4be5df782689bb35d651f385a9b57a3b801a6d7e
alexeenkoff <alexeenkoff@gmail.com>
2020-03-21 17:19:47 +0300

    Add scroll to wrap only

--- lib/src/ui/tags/tags_screen.dart
+++ lib/src/ui/tags/tags_screen.dart
@@ -23,64 +23,60 @@ class _TagsPageState extends State<TagsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).backgroundColor,
-      body: IntrinsicHeight(
+      body: Container(
+        margin: EdgeInsets.only(left: 16.0, right: 16.0, top: 64.0),
         child: Container(
-          margin: EdgeInsets.only(left: 16.0, right: 16.0, top: 64.0),
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: EdgeInsets.only(bottom: 16.0),
-              child: Column(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Expanded(flex: 1, child: TagCounter(key: _tagCounterKey)),
-                      Container(margin: EdgeInsets.only(left: 8, right: 8)),
-                      Expanded(flex: 4, child: TagSearch())
-                    ],
-                  ),
-                  WhenRebuilder<TagService>(
-                    // ignore: missing_return
-                    models: [tagServiceRM],
-                    initState: (_, service) =>
-                        service.setState((s) => s.popularTags()),
-                    // ignore: missing_return
-                    onIdle: () => Container(),
-                    onWaiting: () => CircularProgressIndicator(),
-                    onError: (error) => Container(
-                      width: 100,
-                      height: 100,
-                      decoration: BoxDecoration(color: Colors.blue),
-                    ),
-                    onData: (tagService) {
-                      return StateBuilder(
-                        models: [tagServiceRM],
-                        builder: (_, __) {
-                          return Container(
-                            margin: EdgeInsets.only(top: 24.0),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Wrap(
-                                  alignment: WrapAlignment.center,
-                                  spacing: 8,
-                                  runSpacing: 16,
-                                  children: tagService.tags
-                                      .map((tag) =>
-                                          TagWidget('#' + tag.text, _onTagPress))
-                                      .toList(),
-//                              ],
-                                )
-                              ],
-                            ),
-                          );
-                        },
-                      );
-                    },
-                  ),
+                  Expanded(flex: 1, child: TagCounter(key: _tagCounterKey)),
+                  Container(margin: EdgeInsets.only(left: 8, right: 8)),
+                  Expanded(flex: 4, child: TagSearch())
                 ],
               ),
-            ),
+              WhenRebuilder<TagService>(
+                // ignore: missing_return
+                models: [tagServiceRM],
+                initState: (_, service) =>
+                    service.setState((s) => s.popularTags()),
+                // ignore: missing_return
+                onIdle: () => Container(),
+                onWaiting: () => CircularProgressIndicator(),
+                onError: (error) => Container(
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(color: Colors.blue),
+                ),
+                onData: (tagService) {
+                  return StateBuilder(
+                    models: [tagServiceRM],
+                    builder: (_, __) {
+                      return Expanded(
+                        flex: 1,
+                        child: SingleChildScrollView(
+                          child: Padding(
+                            padding: EdgeInsets.only(top: 24, bottom: 16),
+                            child: Wrap(
+                              alignment: WrapAlignment.center,
+                              spacing: 8,
+                              runSpacing: 16,
+                              children: tagService.tags
+                                  .map((tag) =>
+                                      TagWidget('#' + tag.text, _onTagPress))
+                                  .toList(),
+//                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
+            ],
           ),
         ),
       ),
