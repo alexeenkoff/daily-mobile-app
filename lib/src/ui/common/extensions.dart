70fa62280caac8fb889191a78ad07aa174ce443c
alexeenkoff <alexeenkoff@gmail.com>
2020-04-12 10:53:34 +0300

    Remove unuse code

--- lib/src/ui/common/extensions.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:flutter/foundation.dart';
-
-extension Platform on StatelessWidget {
-  bool isAndroid() {
-    return defaultTargetPlatform == TargetPlatform.android;
-  }
-}
