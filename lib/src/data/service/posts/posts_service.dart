61659046f34b134eb5c2b68b5e5f32fed3596b92
alexeenkoff <alexeenkoff@gmail.com>
2020-04-17 23:35:51 +0300

    Refactor posts_service

--- lib/src/data/service/posts/posts_service.dart
+++ lib/src/data/service/posts/posts_service.dart
@@ -1,10 +1,12 @@
 import 'package:daily_mobile_app/src/domain/entities/post.dart';
+import 'package:daily_mobile_app/src/domain/entities/tag.dart';
 import 'package:daily_mobile_app/src/domain/interfaces/posts_repository.dart';
 import 'package:daily_mobile_app/src/domain/interfaces/tag_repositoty.dart';
 
 class PostsService {
   final TagStorageRepository _tagStorageRepository;
   final PostsRestRepository _postsRestRepository;
+  static const _PAGE_SIZE = 10;
 
   PostsService(this._tagStorageRepository, this._postsRestRepository);
 
@@ -12,14 +14,17 @@ class PostsService {
   var _page = 0;
 
   void initState() async {
-    final savedTags = await _tagStorageRepository.getSelectedTags();
-    posts = await _postsRestRepository.getPosts(savedTags, _page, 10);
+    posts = await _loadPage(_page);
   }
 
   void loadNextPage() async {
     _page++;
-    final savedTags = await _tagStorageRepository.getSelectedTags();
-    final nextPagePosts = await _postsRestRepository.getPosts(savedTags, _page, 10);
+    final nextPagePosts = await _loadPage(_page);
     posts.addAll(nextPagePosts);
+  }
+
+  Future<List<Post>> _loadPage(int page) async {
+    final savedTags = await _tagStorageRepository.getSelectedTags();
+    return await _postsRestRepository.getPosts(savedTags, page, _PAGE_SIZE);
   }
 }
