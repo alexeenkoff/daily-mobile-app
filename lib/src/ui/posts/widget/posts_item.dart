0f8ea13c14db6e50571d730225690cfe0299bbf3
geowatson <georgy.poputnikov@x5.ru>
2020-04-18 21:45:37 +0300

    added design

--- lib/src/ui/posts/widget/posts_item.dart
+++ lib/src/ui/posts/widget/posts_item.dart
@@ -9,15 +9,143 @@ class PostsItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        height: 295,
-//        child: ListTile(title: Text(_post.title)));
-        child: Column(children: [
-          Placeholder(
-            fallbackHeight: 100,
-            color: Theme.of(context).accentColor,
+    final cornerRadius = Radius.circular(10);
+    final cardHeight = 280.0; // estimated card height based on intuition
+    final maxTags = 2;
+    var tags = _post.tags.length > maxTags
+        ? _post.tags
+                .sublist(0, maxTags)
+                .map((v) => "#" + v[0].toUpperCase() + v.substring(1))
+                .join(", ") +
+            ", +${_post.tags.length - maxTags}"
+        : _post.tags
+            .map((v) => "#" + v[0].toUpperCase() + v.substring(1))
+            .join(", ");
+    final publishedAt =
+        DateTime.now().difference(DateTime.parse(_post.publishedAt)).inMinutes;
+    String timeFromPublication;
+    if (publishedAt > 60 * 24 * 7) {
+      timeFromPublication = "${publishedAt ~/ (60 * 24 * 7)}d";
+    } else if (publishedAt > 60 * 24) {
+      timeFromPublication = "${publishedAt ~/ (60 * 24)}d";
+    } else if (publishedAt > 60) {
+      timeFromPublication = "${publishedAt ~/ 60}h";
+    } else {
+      timeFromPublication = "${publishedAt}m";
+    }
+    timeFromPublication += " ago";
+
+    var imageContainer = LayoutBuilder(
+        builder: (BuildContext buildContext, BoxConstraints constraints) {
+      return Container(
+        height: cardHeight / 2,
+        width: constraints.maxWidth,
+        child: ClipRRect(
+          borderRadius:
+              BorderRadius.only(topLeft: cornerRadius, topRight: cornerRadius),
+          child: FittedBox(
+            fit: BoxFit.fitWidth,
+            child: Transform.scale(
+              scale: 1.1,
+              child: Image.network(_post.image),
+            ),
           ),
-          ListTile(title: Text(_post.title)),
-        ]));
+        ),
+      );
+    });
+
+    var infoContainer = Container(
+      padding: EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 15),
+      decoration: BoxDecoration(
+          color: Theme.of(context).primaryColor,
+          borderRadius: BorderRadius.all(cornerRadius)),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(bottom: 20),
+            alignment: Alignment.centerLeft,
+            child: Text(
+              _post.title,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                tags,
+                style: TextStyle(
+                    color: Color.fromARGB(255, 153, 153, 156), fontSize: 12),
+              ),
+              _post.readTime != null
+                  ? Text("${_post?.readTime} min read",
+                      style: TextStyle(
+                          color: Color.fromARGB(255, 153, 153, 156),
+                          fontSize: 12))
+                  : Text(""),
+            ],
+          ),
+          Divider(
+            color: Color.fromARGB(255, 77, 77, 77),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                child: Row(
+                  children: <Widget>[
+                    ClipRRect(
+                      borderRadius: BorderRadius.all(Radius.circular(3)),
+                      child: FittedBox(
+                        fit: BoxFit.fitWidth,
+                        child: Image.network(_post.publicationImage,
+                            width: 25, height: 25),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 10),
+                      child: Text(
+                        timeFromPublication,
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 153, 153, 156),
+                            fontSize: 12,
+                            fontStyle: FontStyle.italic),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Row(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(right: 10),
+                    child: Image.asset(
+                      'assets/images/bookmark.png',
+                      height: 20,
+                      width: 20,
+                    ),
+                  ),
+                  Image.asset(
+                    'assets/images/three_dots.png',
+                    height: 20,
+                    width: 20,
+                  )
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+    );
+
+    return Container(
+        child: Container(
+      decoration: BoxDecoration(
+        color: Theme.of(context).primaryColor,
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+      ),
+      child: Column(children: <Widget>[imageContainer, infoContainer]),
+    ));
   }
 }
