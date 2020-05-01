1cb2eb1e4a9361c3e3b2f7f9689cc3f994f5f117
alexeenkoff <alexeenkoff@gmail.com>
2020-05-01 21:20:41 +0300

    Add whenrebuilder for auth screen

--- lib/src/ui/auth/auth_screen.dart
+++ lib/src/ui/auth/auth_screen.dart
@@ -21,29 +21,30 @@ class _AuthPageState extends State<AuthWebPage> {
     _authServiceWM.value.initState(provider);
     return WhenRebuilder(
       models: [_authServiceWM],
-      onIdle: () => Container(),
+      onIdle: () => buildWebView(cookieManager),
       onData: (_) => Container(),
       onError: (_) => Container(),
       onWaiting: () => Container(),
     );
+  }
 
-//    return WebView(
-//      onWebViewCreated: (controller) {
-//        clearCookies(cookieManager);
-//        controller.clearCache();
-//      },
-//      javascriptMode: JavascriptMode.unrestricted,
-//      initialUrl:
-//      'https://github.com/login?client_id=92d76a890e5c6144a4af&return_to=%2Flogin%2Foauth%2Fauthorize%3Faccess_type%3Doffline%26client_id%3D92d76a890e5c6144a4af%26redirect_uri%3Dhttps%253A%252F%252Fapp.dailynow.co%252Fv1%252Fauth%252Fcallback%26response_type%3Dcode%26scope%3Duser%253Aemail%26state%3D%257B%2522provider%2522%253A%2522github%2522%252C%2522redirect_uri%2522%253A%2522moz-extension%253A%252F%252Ff86cc5f4-01cb-9542-8c9c-41bde03d68b5%252Findex.html%253Fprovider%253Dgithub%2522%252C%2522code_challenge%2522%253A%2522QZnwtMqOpzX3FQwv_d6w1u9sK9n5qg_oaiQIRrjcyZE%2522%257D',
-//      onPageStarted: (url) {
-//        if (url.contains('moz-extension://')) {
-//          _parseRedirect(url);
-//        }
-//      },
-//      onWebResourceError: (error) {
-//        int x = 0;
-//      },
-//    );
+  WebView buildWebView(CookieManager cookieManager) {
+    return WebView(
+      onWebViewCreated: (controller) {
+        clearCookies(cookieManager);
+        controller.clearCache();
+      },
+      javascriptMode: JavascriptMode.unrestricted,
+      initialUrl: _authServiceWM.value.authUrl,
+      onPageStarted: (url) {
+        if (url.contains('moz-extension://')) {
+          _parseRedirect(url);
+        }
+      },
+      onWebResourceError: (error) {
+        int x = 0;
+      },
+    );
   }
 
   void clearCookies(CookieManager manager) async {
