4c74da637f63336b3d969d9625aab93844845db9
alexeenkoff <alexeenkoff@gmail.com>
2020-05-01 23:17:11 +0300

    Add parse of exception

--- lib/src/data/repository/auth/ios_exception_parser.dart
+++ lib/src/data/repository/auth/ios_exception_parser.dart
@@ -1,12 +1,11 @@
 class IOSExceptionParser {
-  final String _exceptionDescription;
+  final String _errorDescription;
 
-  String string =
-      'Error Domain= Code=0 "Redirection to URL with a scheme that is not HTTP(S)" UserInfo={_WKRecoveryAttempterErrorKey=<WKReloadFrameErrorRecoveryAttempter: 0x600002534ea0>, NSErrorFailingURLStringKey=moz-extension://f86cc5f4-01cb-9542-8c9c-41bde03d68b5/index.html?provider=github&code=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1ODgzNTg4MTEuOTgyLCJwcm92aWRlckNvZGUiOiI0MzQ0NGVjZDMxMTA3NzhkMjE5NyIsInByb3ZpZGVyIjoiZ2l0aHViIiwiY29kZUNoYWxsZW5nZSI6IlFabnd0TXFPcHpYM0ZRd3ZfZDZ3MXU5c0s5bjVxZ19vYWlRSVJyamN5WkUiLCJpYXQiOjE1ODgzNTg3NTEsImF1ZCI6IkRhaWx5IiwiaXNzIjoiRGFpbHkgQVBJIn0.CDrD7lqAkUjROx4gZvzxUUzxzE8eo9BUerMb8TMrwkk, NSErrorFailingURLKey=moz-extension://f86cc5f4-01cb-9542-8c9c-41bde03d68b5/index.html?provider=github&code=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1ODgzNTg4MTEuOTgyLCJwcm92aWRlckNvZGUiOiI0MzQ0NGVjZDMxMTA3NzhkMjE5NyIsInByb3ZpZGVyIjoiZ2l0aHViIiwiY29kZUNoYWxsZW5nZSI6IlFabnd0TXFPcHpYM0ZRd3ZfZDZ3MXU5c0s5bjVxZ19vYWlRSVJyamN5WkUiLCJpYXQiOjE1ODgzNTg3NTEsImF1ZCI6IkRhaWx5IiwiaXNzIjoiRGFpbHkgQVBJIn0.CDrD7lqAkUjROx4gZvzxUUzxzE8eo9BUerMb8TMrwkk, NSLocalizedDescription=Redirection to URL with a scheme that is not HTTP(S)}';
-
-  IOSExceptionParser(this._exceptionDescription);
+  IOSExceptionParser(this._errorDescription);
 
   String getUrlFromErrorDescription() {
-    return string;
+    var regExp = RegExp('moz-extension:([^,]+)');
+    var firstMatch = regExp.firstMatch(_errorDescription);
+    return _errorDescription.substring(firstMatch.start, firstMatch.end);
   }
 }
--- test/data/repository/auth/ios_exception_parser_test.dart
+++ test/data/repository/auth/ios_exception_parser_test.dart
@@ -4,13 +4,13 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   test('parse ios deeplink exception', () {
     final correctUrl =
-        'moz-extension://f86cc5f4-01cb-9542-8c9c-41bde03d68b5/index.html?provider=github&code=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1ODgzNTg4MTEuOTgyLCJwcm92aWRlckNvZGUiOiI0MzQ0NGVjZDMxMTA3NzhkMjE5NyIsInByb3ZpZGVyIjoiZ2l0aHViIiwiY29kZUNoYWxsZW5nZSI6IlFabnd0TXFPcHpYM0ZRd3ZfZDZ3MXU5c0s5bjVxZ19vYWlRSVJyamN5WkUiLCJpYXQiOjE1ODgzNTg3NTEsImF1ZCI6IkRhaWx5IiwiaXNzIjoiRGFpbHkgQVBJIn0.CDrD7lqAkUjROx4gZvzxUUzxzE8eo9BUerMb8TMrwkk, NSErrorFailingURLKey=moz-extension://f86cc5f4-01cb-9542-8c9c-41bde03d68b5/index.html?provider=github&code=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1ODgzNTg4MTEuOTgyLCJwcm92aWRlckNvZGUiOiI0MzQ0NGVjZDMxMTA3NzhkMjE5NyIsInByb3ZpZGVyIjoiZ2l0aHViIiwiY29kZUNoYWxsZW5nZSI6IlFabnd0TXFPcHpYM0ZRd3ZfZDZ3MXU5c0s5bjVxZ19vYWlRSVJyamN5WkUiLCJpYXQiOjE1ODgzNTg3NTEsImF1ZCI6IkRhaWx5IiwiaXNzIjoiRGFpbHkgQVBJIn0.CDrD7lqAkUjROx4gZvzxUUzxzE8eo9BUerMb8TMrwkk';
+        'moz-extension://f86cc5f4-01cb-9542-8c9c-41bde03d68b5/index.html?provider=github&code=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1ODgzNTg4MTEuOTgyLCJwcm92aWRlckNvZGUiOiI0MzQ0NGVjZDMxMTA3NzhkMjE5NyIsInByb3ZpZGVyIjoiZ2l0aHViIiwiY29kZUNoYWxsZW5nZSI6IlFabnd0TXFPcHpYM0ZRd3ZfZDZ3MXU5c0s5bjVxZ19vYWlRSVJyamN5WkUiLCJpYXQiOjE1ODgzNTg3NTEsImF1ZCI6IkRhaWx5IiwiaXNzIjoiRGFpbHkgQVBJIn0.CDrD7lqAkUjROx4gZvzxUUzxzE8eo9BUerMb8TMrwkk';
 
     final redirectErrorDescription =
-        'Error Domain= Code=0 "Redirection to URL with a scheme that is not HTTP(S)" UserInfo={_WKRecoveryAttempterErrorKey=<WKReloadFrameErrorRecoveryAttempter: 0x600002534ea0>, NSErrorFailingURLStringKey=$correctUrl, NSLocalizedDescription=Redirection to URL with a scheme that is not HTTP(S)}';
+        'Error Domain= Code=0 "Redirection to URL with a scheme that is not HTTP(S)" UserInfo={_WKRecoveryAttempterErrorKey=<WKReloadFrameErrorRecoveryAttempter: 0x600002534ea0>, NSErrorFailingURLStringKey=$correctUrl, NSErrorFailingURLKey=moz-extension://f86cc5f4-01cb-9542-8c9c-41bde03d68b5/index.html?provider=github&code=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1ODgzNTg4MTEuOTgyLCJwcm92aWRlckNvZGUiOiI0MzQ0NGVjZDMxMTA3NzhkMjE5NyIsInByb3ZpZGVyIjoiZ2l0aHViIiwiY29kZUNoYWxsZW5nZSI6IlFabnd0TXFPcHpYM0ZRd3ZfZDZ3MXU5c0s5bjVxZ19vYWlRSVJyamN5WkUiLCJpYXQiOjE1ODgzNTg3NTEsImF1ZCI6IkRhaWx5IiwiaXNzIjoiRGFpbHkgQVBJIn0.CDrD7lqAkUjROx4gZvzxUUzxzE8eo9BUerMb8TMrwkk, NSLocalizedDescription=Redirection to URL with a scheme that is not HTTP(S)}';
 
     final exceptionParser = IOSExceptionParser(redirectErrorDescription);
-
-    assert(exceptionParser.getUrlFromErrorDescription() == correctUrl);
+    var parsedUrl = exceptionParser.getUrlFromErrorDescription();
+    assert(parsedUrl == correctUrl);
   });
 }
